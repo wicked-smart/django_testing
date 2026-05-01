b8855bb3ba71826cecc107c68dc3d87b2df1493b
wicked-smart <marzaanvan@gmail.com>
2026-05-01 22:21:10 +0530

    update views.py

--- flight_book/views.py
+++ flight_book/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Welcome back to django developement !!!")
+    return HttpResponse("What's up man! how's life going ..having fun doing this software developement thing!!!")
