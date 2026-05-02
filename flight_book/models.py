2bde3e6d23bbed7634a20508c1410440181c42ad
wicked-smart <marzaanvan@gmail.com>
2026-05-02 16:05:16 +0530

    Add to models.py

--- flight_book/models.py
+++ flight_book/models.py
@@ -2,6 +2,34 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-
 class User(AbstractUser):
     pass 
+
+
+class Airport(models.Model):
+    city = models.CharField(max_length=100)
+    code =  models.CharField(max_length=3)
+
+    def __init__(self):
+        return f"{self.city}({self.code})"
+
+class Flight(models.Modell):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='departures')
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name='arrivals')
+    duration = models.IntegerField(default=230)
+
+    def __init__(self):
+        return f"{self.origin.city} --> {self.destination.city}"
+
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    flight = models.ManyToManyField(Flight, related_name='passengers')
+
+    def __init__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+
+
+
